29f74d44c991cb41781a52679d451b366580ff60
Taiwo <taiwoadeeola@gmail.com>
2023-06-21 10:48:18 -0700

    1-alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,14 +5,18 @@
  * Return: Always 0 Success
  */
 void print_alphabet(void)
-{	
-	char letter  = 'a';
-	while (letter  <= 'z')
+{
+	char letter = 'a';
+	{
+	while (letter <= 'z')
 	{
 		_putchar(letter);
 		letter++;
 	}
 
 	_putchar('\n');
+
+	}
+
 }
 

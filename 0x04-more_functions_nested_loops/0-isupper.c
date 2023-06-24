d9b460bf76357a8a978778a97e13aad0563f9765
Taiwo <taiwoadeeola@gmail.com>
2023-06-24 08:18:54 -0700

    numbers

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
 /**
- * _isupper -check if a letter is upper 
- * @x: the number to be checked
- * Return: 1 for upper or 0 for any else
- */
+* _isupper - check if a letter is upper
+* @x: the number to be checked
+* Return: 1 for upper or 0 for any else
+*/
 
 int _isupper(int x)
 {
-	if (x >= :65 && 'x' <= 90);
-	{
-		return (1);
-	}
-	return (0);
+
+if (x >= 65 && 'x' <= 90);
+{
+return (1);
+}
+return (0);
 }
--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,24 +1,23 @@
 #include "main.h"
 
 /**
- * more_numbers - prints more numbers
- *
- */
-void more_numbers(void)
+* print_most_numbers - print num except 2 and 4
+*
+* Return: void
+*/
+
+void print_most_numbers(void)
+
 {
-	int i, j;
 
-	for (i < 10; i++) 
-	{
-	for (j = 0; j <= 14; j++)
-	{
-		if (j >= 10)
-		_putchar('1');
-		_putchar(j % 10 + '0');
-	}
-	_putchar('\n');
+char c;
+
+for (c = '0'; c <= '9'; c++)
+{
+if (!(c == '2' || c == '4'))
+_putchar(c);
 }
-		     
+_putchar('\n');
 
-
+}
 

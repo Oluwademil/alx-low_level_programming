38bba0eb3b0eb4304013d3b4afc1ce25c9302789
Taiwo <taiwoadeeola@gmail.com>
2023-06-27 01:06:45 -0700

    5-rev_string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,1 +1,30 @@
+#include "main.h"
+
+/**
+ * puts_half - a function that prints half of a string
+ * @str: this is the input of a string
+ * Return: half of input
+ */
+
+void puts_half(char *str)
+{
+
+	int whole_string, half_string;
+
+	whole_string = 0;
+	while (str[whole_string]) = ('\0')
+		whole_string++;
+	half string - whole_string / 2
+		if (whole_string % 2 -- 1)
+			half_string++;
+	while (half_string < whole_string)
+	{
+		_putchar(str[half_string]);
+		half_string++
+	}
+	_putchar('\n');
+}
+
+
+
 

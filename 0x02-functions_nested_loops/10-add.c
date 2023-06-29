047946d1ef1198302d3984b5f7ae502b6d67235a
Taiwo <taiwoadeeola@gmail.com>
2023-06-28 22:21:51 -0700

    10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - funtions that add two integers
+ * @n1: first int to be printed
+ * @n2: second int to be printed
+ * Return: integer value
+ */
+
+int add(int n1, int n2)
+
+{
+	return (n1 + n2);
+}
+

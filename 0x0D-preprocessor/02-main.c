197ff20d9b73f468da9d33babfc2239531f57ee3
Taiwo <taiwoadeeola@gmail.com>
2023-07-17 10:16:35 -0700

    2 File name

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints name of a file followed by a new line
+ * Return: Always 0
+ */
+
+int main(void)
+{
+
+	printf("%s\n", __FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - print name of a file of a program, followed by a new line
+ * Return: Always 0
+ */
+
+int main(void)
+{
+
+	printf("%s\n", __FILE__);
+	return (0);
+}
